447a648ebf924901e1195e82a027942f44dd14f9
Thomas <tommyprezioso@gmail.com>
2023-06-03 14:45:23 -0400

    fixing url

--- Sources/SwiftTom/main.swift
+++ Sources/SwiftTom/main.swift
@@ -18,7 +18,7 @@ struct SwiftTom: Website {
     }
 
     // Update these properties to configure your website:
-    var url = URL(string: "https://tprezioso.github.io/SwiftTom/")!
+    var url = URL(string: "https://tprezioso.github.io/")!
     var name = "SwiftTom"
     var description = ""
 //    var description = "Hi! My name is Tom, I am a self taught programmer that has been programming since 2015. This blog is to help teach swift programming topics to anyone who wants to learn how to program in swift. Topics covered on this blog will range from beginner to experienced. Come join me in learning all about the amazing programming language that is swift!"
@@ -42,13 +42,13 @@ try SwiftTom().publish(using: [
             config: config
         )
     },
-    .generateSiteMap(),
-    
     .deploy(using: .gitHub(
-                "Tprezioso/SwiftTom",
-                branch: "main",
-                useSSH: false)
-            )
+        "Tprezioso/SwiftTom",
+        branch: "main",
+        useSSH: false)
+    ),
+    .generateSiteMap()
+    
   ])
     
     
