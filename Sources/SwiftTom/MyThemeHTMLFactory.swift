875b5228cf4ab2a7e84da7522934c650000a1734
Thomas <tommyprezioso@gmail.com>
2023-06-04 17:28:17 -0400

    changing extension

--- Sources/SwiftTom/MyThemeHTMLFactory.swift
+++ Sources/SwiftTom/MyThemeHTMLFactory.swift
@@ -9,7 +9,7 @@ import Plot
 import Publish
 import Foundation
 
-public extension Theme {
+public extension Theme where Site == SwiftTom {
     /// The default "Foundation" theme that Publish ships with, a very
     /// basic theme mostly implemented for demonstration purposes.
     static var myTheme: Self {
