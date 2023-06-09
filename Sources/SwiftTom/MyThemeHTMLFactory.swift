8ca26b1dfdad2a5562d5ae7d45263cc80605fcce
Thomas <tommyprezioso@gmail.com>
2023-06-09 14:59:15 -0400

    still trying here

--- Sources/SwiftTom/MyThemeHTMLFactory.swift
+++ Sources/SwiftTom/MyThemeHTMLFactory.swift
@@ -157,7 +157,7 @@ public struct MyThemeHTMLFactory<Site: Website>: HTMLFactory {
 
     var body: Component {
         Header {
-            Image(url:"images/tom.jpg", description: "Tom")
+            Image(url:"resources/images/tom.jpg", description: "Tom")
                 .class("author-avatar")
                 
             Wrapper {
